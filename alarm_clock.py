86899f6b6ec163dcc3f25bc560d7b67db00a4ccc
blue-slushy9 <ra.cruz971@gmail.com>
2023-04-28 21:55:52 -0700

    started building out the code more

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,15 +1,45 @@
-import pytz, datetime
+import sched, time
 
-tz = pytz.timezone('US/Pacific')
+#tz = pytz.timezone('US/Pacific')
 
-current_time = datetime.datetime.now(tz)
+#current_time = datetime.datetime.now(tz)
 
 print("This is an alarm clock app! Please enter the time you wish for your alarm to go off, either AM/PM or 2400 format is fine.")
 
-alarm_time = input()
+alarm_time = input().lower()
 print()
 
 print("Do you want this alarm to go off every day, or just once?")
+
+alarm_frequency = input().lower()
 print()
 
-print(current_time)
+#print(current_time)
+
+# Define the alarm function
+
+def alarm():
+    print(f"Ring! It's {alarm_time}, time to wake up!")
+
+# Create a scheduler object
+
+scheduler = sched.scheduler(time.time, time.sleep)
+
+# Schedule the function to run at specified time
+
+alarm_list = []
+
+if 'am' in alarm_time:
+    for char in alarm_time:
+        alarm_list.append(char)
+    for char in 'am':
+        alarm_list.remove(char)
+    if len(alarm_list) == 1:
+        alarm_time = time.strptime(f'0{alarm_list[0]}:00', '%H:%M')
+        alarm_time = time.mktime(alarm_time)
+        scheduler.enterabs(alarm_time, 1, alarm)
+
+
+
+
+scheduler.run()
