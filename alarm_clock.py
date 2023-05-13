860d9682b5c33ca403c7242b9da596e6eec5c38e
blue-slushy9 <ra.cruz971@gmail.com>
2023-05-13 07:14:38 +0000

    I think the code is pretty much finished for now! I think I could probably go back and improve the comments, but it is getting late so I will do that tomorrow...

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+print("This is an alarm clock app! Please enter the time you want for your alarm to ring. Please use the format 00:00AM or PM; military time is also acceptable, e.g. 18:00 for 06:00PM.")
+
 alarm_time = input().upper()
 print()
 
@@ -20,11 +22,11 @@ def alarm_execution(alarm_time):
     # Create an alarm_time2 variable that will store the current datetime; then we use the replace() function to 
     # overwrite the hour, minutes, etc. with our target time (the alarm time per user input); then we use the 
     # timedelta class to add x days to our alarm_time2, e.g. if we add 1 day the alarm will go off tomorrow;
-    alarm_time2 = datetime.datetime.now().replace(hour=HH, minute=MM, second=0, microsecond=0)
+    alarm_time2 = datetime.now().replace(hour=HH, minute=MM, second=0, microsecond=0)
     
     # Create a while loop that will constantly check the datetime.now, then assign it to the variable now; 
     while True:
-        now = datetime.datetime.now()
+        now = datetime.now()
     
         # Create an if statement inside of the while loop that will compare the now variable to the alarm time per
         # user input; if the now variable matches or exceeds the datetime of the input alarm time...
